ebed151a7d32718842486f7066eccf297d2cb7d8
Alex Lee <alexleegk@gmail.com>
2018-11-24 20:41:19 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='lpips_tf',
+      version='0.1',
+      description='Tensorflow port for the Learned Perceptual Image Patch Similarity (LPIPS) metric.',
+      author='Alex Lee',
+      url='https://github.com/alexlee-gk/lpips-tensorflow/',
+      license='BSD 2-Clause License',
+      py_modules=['lpips'],
+     )
